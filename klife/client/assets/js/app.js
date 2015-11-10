2ed14977aa21ffba252e4ed06aa552f4b2253c89
zna-com <znacloud@126.com>
2015-11-10 19:24:38 +0800

    modify logo page

--- klife/client/assets/js/app.js
+++ klife/client/assets/js/app.js
@@ -1,51 +1,63 @@
-+function ($) {
-$.fn.linkAllCircle = function() {
-    'use strict';
+ jQuery.extend({
+    linkAllCircle: function(callback) {
+        'use strict';
+        $(".k-circle").animate({left:"+=213px"},"fast");
+        $(".l-circle").animate({left:"+=107px"},"fast");
+        $(".f-circle").animate({left:"-=107px"},"fast");
+        $(".e-circle").animate({left:"-=213px"},"fast",callback);
+     }
+,
+    showECircle: function(callback) {
+        'use strict';
+        $(".e-circle").animate({
+            left: '-80px',
+            opacity: '1'
+        }, 2500, callback);
+    }
+,
+    showFCircle: function(callback) {
+        'use strict';
+        $(".f-circle").animate({
+            left: '-40px',
+            opacity: '1'
+        }, 2500,callback);
+    }
+,
+    showICircle: function(callback) {
+        'use strict';
+        $(".i-circle").animate({
+            left: '0px',
+            opacity: '1'
+        }, 2500, callback);
 }
-
-$.fn.showECircle = function() {
-    'use strict';
-    $(".e-circle").animate({
-        top: '0px',
-        opacity: '1'
-    }, 3000, linkAllCircle);
+,
+    showLCircle: function(callback) {
+        'use strict';
+        $(".l-circle").animate({
+            left: '40px',
+            opacity: '1'
+        }, 2500, callback);
 }
-
-$.fn.showFCircle = function() {
-    'use strict';
-    $(".f-circle").animate({
-        top: '0px',
-        opacity: '1'
-    }, 3000, showECircle);
-}
-
-$.fn.showICircle = function() {
-    'use strict';
-    $(".i-circle").animate({
-        top: '0px',
-        opacity: '1'
-    }, 3000, showFCircle);
-}
-
-$.fn.showLCircle = function() {
-    'use strict';
-    $(".l-circle").animate({
-        top: '0px',
-        opacity: '1'
-    }, 3000, showICircle);
-}
-
-$.fn.showKCircle = function() {
-    'use strict';
+,
+    showKCircle: function(callback) {
+        'use strict';
     
-    $(".k-circle").animate({
-        top: '0px',
-        opacity: '1'
-    }, 3000, showLCircle);
-}
-}(jQuery);
-
-
+        $(".k-circle").animate({
+            left: '80px',
+            opacity: '1'
+        }, 2500, callback);
+    }
+,
+     hiddenAllCircles: function(callback) {
+         'use strict';
+         $(".k-circle").animate({opacity:"0"},"slow");
+        $(".l-circle").animate({opacity:"0"},"slow");
+        $(".i-circle").animate({opacity:"0"},"slow");
+        $(".f-circle").animate({opacity:"0"},"slow");
+        $(".e-circle").animate({opacity:"0"},"slow");
+        $(".mix-circle").animate({opacity:"1"},"slow",callback);
+     }
+});
 
 
 
